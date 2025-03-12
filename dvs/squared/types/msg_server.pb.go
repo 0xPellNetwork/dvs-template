b53473b553985a840206ecc3bf71aa3ee05c5c5d
jacksoom <lifengliu1994@gmail.com>
2025-03-12 13:20:24 +0800

    remove unused proto

--- dvs/squared/types/msg_server.pb.go
+++ dvs/squared/types/msg_server.pb.go
@@ -77,50 +77,6 @@ func (m *RequestNumberSquaredIn) GetTask() *TaskRequest {
 	return nil
 }
 
-type RequestNumberSquaredInResp struct {
-	Task *TaskRequest `protobuf:"bytes,1,opt,name=task,proto3" json:"task,omitempty"`
-}
-
-func (m *RequestNumberSquaredInResp) Reset()         { *m = RequestNumberSquaredInResp{} }
-func (m *RequestNumberSquaredInResp) String() string { return proto.CompactTextString(m) }
-func (*RequestNumberSquaredInResp) ProtoMessage()    {}
-func (*RequestNumberSquaredInResp) Descriptor() ([]byte, []int) {
-	return fileDescriptor_e7c06820b8d1af06, []int{1}
-}
-func (m *RequestNumberSquaredInResp) XXX_Unmarshal(b []byte) error {
-	return m.Unmarshal(b)
-}
-func (m *RequestNumberSquaredInResp) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	if deterministic {
-		return xxx_messageInfo_RequestNumberSquaredInResp.Marshal(b, m, deterministic)
-	} else {
-		b = b[:cap(b)]
-		n, err := m.MarshalToSizedBuffer(b)
-		if err != nil {
-			return nil, err
-		}
-		return b[:n], nil
-	}
-}
-func (m *RequestNumberSquaredInResp) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_RequestNumberSquaredInResp.Merge(m, src)
-}
-func (m *RequestNumberSquaredInResp) XXX_Size() int {
-	return m.Size()
-}
-func (m *RequestNumberSquaredInResp) XXX_DiscardUnknown() {
-	xxx_messageInfo_RequestNumberSquaredInResp.DiscardUnknown(m)
-}
-
-var xxx_messageInfo_RequestNumberSquaredInResp proto.InternalMessageInfo
-
-func (m *RequestNumberSquaredInResp) GetTask() *TaskRequest {
-	if m != nil {
-		return m.Task
-	}
-	return nil
-}
-
 // RequestPriceFeedOut
 type RequestNumberSquaredOut struct {
 	TaskIndex uint32 `protobuf:"varint,1,opt,name=task_index,json=taskIndex,proto3" json:"task_index,omitempty"`
@@ -132,7 +88,7 @@ func (m *RequestNumberSquaredOut) Reset()         { *m = RequestNumberSquaredOut
 func (m *RequestNumberSquaredOut) String() string { return proto.CompactTextString(m) }
 func (*RequestNumberSquaredOut) ProtoMessage()    {}
 func (*RequestNumberSquaredOut) Descriptor() ([]byte, []int) {
-	return fileDescriptor_e7c06820b8d1af06, []int{2}
+	return fileDescriptor_e7c06820b8d1af06, []int{1}
 }
 func (m *RequestNumberSquaredOut) XXX_Unmarshal(b []byte) error {
 	return m.Unmarshal(b)
@@ -175,7 +131,7 @@ func (m *ResponseNumberSquaredOut) Reset()         { *m = ResponseNumberSquaredO
 func (m *ResponseNumberSquaredOut) String() string { return proto.CompactTextString(m) }
 func (*ResponseNumberSquaredOut) ProtoMessage()    {}
 func (*ResponseNumberSquaredOut) Descriptor() ([]byte, []int) {
-	return fileDescriptor_e7c06820b8d1af06, []int{3}
+	return fileDescriptor_e7c06820b8d1af06, []int{2}
 }
 func (m *ResponseNumberSquaredOut) XXX_Unmarshal(b []byte) error {
 	return m.Unmarshal(b)
@@ -206,7 +162,6 @@ var xxx_messageInfo_ResponseNumberSquaredOut proto.InternalMessageInfo
 
 func init() {
 	proto.RegisterType((*RequestNumberSquaredIn)(nil), "dvs.squared.RequestNumberSquaredIn")
-	proto.RegisterType((*RequestNumberSquaredInResp)(nil), "dvs.squared.RequestNumberSquaredInResp")
 	proto.RegisterType((*RequestNumberSquaredOut)(nil), "dvs.squared.RequestNumberSquaredOut")
 	proto.RegisterType((*ResponseNumberSquaredOut)(nil), "dvs.squared.ResponseNumberSquaredOut")
 }
@@ -214,7 +169,7 @@ func init() {
 func init() { proto.RegisterFile("dvs/squared/msg_server.proto", fileDescriptor_e7c06820b8d1af06) }
 
 var fileDescriptor_e7c06820b8d1af06 = []byte{
-	// 418 bytes of a gzipped FileDescriptorProto
+	// 403 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x92, 0x49, 0x29, 0x2b, 0xd6,
 	0x2f, 0x2e, 0x2c, 0x4d, 0x2c, 0x4a, 0x4d, 0xd1, 0xcf, 0x2d, 0x4e, 0x8f, 0x2f, 0x4e, 0x2d, 0x2a,
 	0x4b, 0x2d, 0xd2, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0xe2, 0x4e, 0x29, 0x2b, 0xd6, 0x83, 0xca,
@@ -227,21 +182,20 @@ var fileDescriptor_e7c06820b8d1af06 = []byte{
 	0xc9, 0xa9, 0x7a, 0x21, 0x89, 0xc5, 0xd9, 0x50, 0x6d, 0x41, 0x60, 0x55, 0x56, 0xc6, 0xa7, 0xb6,
 	0xe8, 0x42, 0x9d, 0xaa, 0x97, 0x94, 0x58, 0x9c, 0xaa, 0x57, 0x66, 0x98, 0x94, 0x5a, 0x92, 0x68,
 	0xa8, 0xe7, 0x5b, 0x9c, 0xde, 0xf5, 0x7c, 0x83, 0x96, 0x30, 0xb2, 0xfd, 0x45, 0x10, 0xad, 0x4a,
-	0xf5, 0x5c, 0x52, 0xd8, 0x2d, 0x0f, 0x4a, 0x2d, 0x2e, 0xa0, 0xad, 0x03, 0x0a, 0xf2, 0xf3, 0x8a,
-	0x53, 0x95, 0xfa, 0x18, 0xb9, 0xc4, 0xb1, 0xb9, 0xc0, 0xbf, 0xb4, 0x44, 0x48, 0x96, 0x8b, 0x0b,
-	0x64, 0x70, 0x7c, 0x66, 0x5e, 0x4a, 0x6a, 0x05, 0xd8, 0x11, 0xbc, 0x41, 0x9c, 0x20, 0x11, 0x4f,
-	0x90, 0x80, 0x90, 0x39, 0x17, 0x3b, 0xd4, 0x34, 0x09, 0x66, 0x05, 0x46, 0x0d, 0x4e, 0x27, 0xd9,
-	0x13, 0xf7, 0xe4, 0x19, 0x6e, 0xdd, 0x93, 0x17, 0x85, 0x38, 0xa2, 0x38, 0x25, 0x5b, 0x2f, 0x33,
-	0x5f, 0x3f, 0x37, 0xb1, 0x24, 0x43, 0xcf, 0x33, 0xaf, 0x24, 0x08, 0xa6, 0xda, 0x4a, 0x1a, 0x8f,
-	0x43, 0x95, 0xa4, 0xb8, 0x24, 0x40, 0x7e, 0x07, 0x39, 0x0e, 0xdd, 0x41, 0x46, 0xb7, 0x18, 0xb9,
-	0x04, 0xa0, 0x5c, 0xdf, 0xe2, 0xf4, 0x60, 0x70, 0x3a, 0x12, 0x4a, 0xe6, 0x12, 0xc1, 0xe6, 0x01,
-	0x21, 0x65, 0x94, 0xe0, 0xc2, 0x1e, 0xca, 0x52, 0x2a, 0x04, 0x15, 0xf9, 0x97, 0x96, 0x28, 0x31,
-	0x08, 0x25, 0x72, 0x09, 0xba, 0x84, 0x05, 0x43, 0x1d, 0xe6, 0x91, 0x98, 0x97, 0x92, 0x93, 0x5a,
-	0x44, 0x9c, 0x0d, 0xaa, 0x68, 0x8a, 0xb0, 0x7b, 0x4d, 0x89, 0xc1, 0x49, 0xfb, 0xc4, 0x23, 0x39,
-	0xc6, 0x0b, 0x8f, 0xe4, 0x18, 0x1f, 0x3c, 0x92, 0x63, 0x9c, 0xf0, 0x58, 0x8e, 0xe1, 0xc2, 0x63,
-	0x39, 0x86, 0x1b, 0x8f, 0xe5, 0x18, 0xa2, 0x04, 0x51, 0x52, 0x6e, 0x65, 0x41, 0x6a, 0x71, 0x12,
-	0x1b, 0x38, 0xed, 0x1a, 0x03, 0x02, 0x00, 0x00, 0xff, 0xff, 0xbd, 0x00, 0xa4, 0x25, 0x5d, 0x03,
-	0x00, 0x00,
+	0x7d, 0x8c, 0x5c, 0xe2, 0xd8, 0x6c, 0xf7, 0x2f, 0x2d, 0x11, 0x92, 0xe5, 0xe2, 0x02, 0x19, 0x1c,
+	0x9f, 0x99, 0x97, 0x92, 0x5a, 0x01, 0x76, 0x04, 0x6f, 0x10, 0x27, 0x48, 0xc4, 0x13, 0x24, 0x20,
+	0x64, 0xce, 0xc5, 0x0e, 0x35, 0x4d, 0x82, 0x59, 0x81, 0x51, 0x83, 0xd3, 0x49, 0xf6, 0xc4, 0x3d,
+	0x79, 0x86, 0x5b, 0xf7, 0xe4, 0x45, 0x21, 0x8e, 0x28, 0x4e, 0xc9, 0xd6, 0xcb, 0xcc, 0xd7, 0xcf,
+	0x4d, 0x2c, 0xc9, 0xd0, 0xf3, 0xcc, 0x2b, 0x09, 0x82, 0xa9, 0xb6, 0x92, 0xc6, 0xe3, 0x50, 0x25,
+	0x29, 0x2e, 0x89, 0xa0, 0xd4, 0xe2, 0x82, 0xfc, 0xbc, 0xe2, 0x54, 0x74, 0x07, 0x19, 0xdd, 0x62,
+	0xe4, 0x12, 0x80, 0x72, 0x7d, 0x8b, 0xd3, 0x83, 0xc1, 0xd1, 0x28, 0x94, 0xcc, 0x25, 0x82, 0xcd,
+	0x03, 0x42, 0xca, 0x28, 0xc1, 0x85, 0x3d, 0x84, 0xa5, 0x54, 0x08, 0x2a, 0xf2, 0x2f, 0x2d, 0x51,
+	0x62, 0x10, 0x4a, 0xe4, 0x12, 0x74, 0x09, 0x0b, 0x86, 0x3a, 0xcc, 0x23, 0x31, 0x2f, 0x25, 0x27,
+	0xb5, 0x88, 0x38, 0x1b, 0x54, 0xd1, 0x14, 0x61, 0xf7, 0x9a, 0x12, 0x83, 0x93, 0xf6, 0x89, 0x47,
+	0x72, 0x8c, 0x17, 0x1e, 0xc9, 0x31, 0x3e, 0x78, 0x24, 0xc7, 0x38, 0xe1, 0xb1, 0x1c, 0xc3, 0x85,
+	0xc7, 0x72, 0x0c, 0x37, 0x1e, 0xcb, 0x31, 0x44, 0x09, 0xa2, 0x24, 0x9c, 0xca, 0x82, 0xd4, 0xe2,
+	0x24, 0x36, 0x70, 0xd2, 0x31, 0x06, 0x04, 0x00, 0x00, 0xff, 0xff, 0xa6, 0x11, 0xfb, 0xc9, 0xdc,
+	0x02, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -345,6 +299,7 @@ func _SquaredMsgServer_DVSResponsHandler_Handler(srv interface{}, ctx context.Co
 	return interceptor(ctx, in, info, handler)
 }
 
+var SquaredMsgServer_serviceDesc = _SquaredMsgServer_serviceDesc
 var _SquaredMsgServer_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "dvs.squared.SquaredMsgServer",
 	HandlerType: (*SquaredMsgServerServer)(nil),
@@ -378,41 +333,6 @@ func (m *RequestNumberSquaredIn) MarshalTo(dAtA []byte) (int, error) {
 }
 
 func (m *RequestNumberSquaredIn) MarshalToSizedBuffer(dAtA []byte) (int, error) {
-	i := len(dAtA)
-	_ = i
-	var l int
-	_ = l
-	if m.Task != nil {
-		{
-			size, err := m.Task.MarshalToSizedBuffer(dAtA[:i])
-			if err != nil {
-				return 0, err
-			}
-			i -= size
-			i = encodeVarintMsgServer(dAtA, i, uint64(size))
-		}
-		i--
-		dAtA[i] = 0xa
-	}
-	return len(dAtA) - i, nil
-}
-
-func (m *RequestNumberSquaredInResp) Marshal() (dAtA []byte, err error) {
-	size := m.Size()
-	dAtA = make([]byte, size)
-	n, err := m.MarshalToSizedBuffer(dAtA[:size])
-	if err != nil {
-		return nil, err
-	}
-	return dAtA[:n], nil
-}
-
-func (m *RequestNumberSquaredInResp) MarshalTo(dAtA []byte) (int, error) {
-	size := m.Size()
-	return m.MarshalToSizedBuffer(dAtA[:size])
-}
-
-func (m *RequestNumberSquaredInResp) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	i := len(dAtA)
 	_ = i
 	var l int
@@ -517,19 +437,6 @@ func (m *RequestNumberSquaredIn) Size() (n int) {
 	return n
 }
 
-func (m *RequestNumberSquaredInResp) Size() (n int) {
-	if m == nil {
-		return 0
-	}
-	var l int
-	_ = l
-	if m.Task != nil {
-		l = m.Task.Size()
-		n += 1 + l + sovMsgServer(uint64(l))
-	}
-	return n
-}
-
 func (m *RequestNumberSquaredOut) Size() (n int) {
 	if m == nil {
 		return 0
@@ -586,92 +493,6 @@ func (m *RequestNumberSquaredIn) Unmarshal(dAtA []byte) error {
 		}
 		if fieldNum <= 0 {
 			return fmt.Errorf("proto: RequestNumberSquaredIn: illegal tag %d (wire type %d)", fieldNum, wire)
-		}
-		switch fieldNum {
-		case 1:
-			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field Task", wireType)
-			}
-			var msglen int
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowMsgServer
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				msglen |= int(b&0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-			if msglen < 0 {
-				return ErrInvalidLengthMsgServer
-			}
-			postIndex := iNdEx + msglen
-			if postIndex < 0 {
-				return ErrInvalidLengthMsgServer
-			}
-			if postIndex > l {
-				return io.ErrUnexpectedEOF
-			}
-			if m.Task == nil {
-				m.Task = &TaskRequest{}
-			}
-			if err := m.Task.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
-				return err
-			}
-			iNdEx = postIndex
-		default:
-			iNdEx = preIndex
-			skippy, err := skipMsgServer(dAtA[iNdEx:])
-			if err != nil {
-				return err
-			}
-			if (skippy < 0) || (iNdEx+skippy) < 0 {
-				return ErrInvalidLengthMsgServer
-			}
-			if (iNdEx + skippy) > l {
-				return io.ErrUnexpectedEOF
-			}
-			iNdEx += skippy
-		}
-	}
-
-	if iNdEx > l {
-		return io.ErrUnexpectedEOF
-	}
-	return nil
-}
-func (m *RequestNumberSquaredInResp) Unmarshal(dAtA []byte) error {
-	l := len(dAtA)
-	iNdEx := 0
-	for iNdEx < l {
-		preIndex := iNdEx
-		var wire uint64
-		for shift := uint(0); ; shift += 7 {
-			if shift >= 64 {
-				return ErrIntOverflowMsgServer
-			}
-			if iNdEx >= l {
-				return io.ErrUnexpectedEOF
-			}
-			b := dAtA[iNdEx]
-			iNdEx++
-			wire |= uint64(b&0x7F) << shift
-			if b < 0x80 {
-				break
-			}
-		}
-		fieldNum := int32(wire >> 3)
-		wireType := int(wire & 0x7)
-		if wireType == 4 {
-			return fmt.Errorf("proto: RequestNumberSquaredInResp: wiretype end group for non-group")
-		}
-		if fieldNum <= 0 {
-			return fmt.Errorf("proto: RequestNumberSquaredInResp: illegal tag %d (wire type %d)", fieldNum, wire)
 		}
 		switch fieldNum {
 		case 1:
